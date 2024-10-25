2f66407a1aa07909bc9c97020b31b2dcb05dceb3
Leonardo José da Silva <124636951+Leonardojdss@users.noreply.github.com>
2024-10-25 17:08:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,24 @@ def generate_image(prompt, usuario):
     quality="standard",
     n=1,
   )
+  print(response.data)
   return response.data[0].url
 
 st.title("Play Profético")
 st.write("Peça ao espirito santo uma palavra profética e fale o que ele te mostrar")
 
-user_input = st.text_input("o que o espirito santo te mostrou?")
+user_input = st.text_input("Digite suas palavras proféticas:")
 
-if user_input:
-  prompt_engineer = "A partir de agora você é artista de artes proféticas, irá receber \
-  algumas palavras proféticas e deverá desenhar de acordo com o usuario dizer"
-  image_url = generate_image(prompt_engineer, user_input)
-  st.write("Aqui está a imagem que o espirito santo te mostrou, se não gostar, recarregua a pagina e tente novamente")
-  st.image(image_url, width=700)
+if st.button("Enviar"):
+    if user_input:
+        prompt_engineer = "A partir de agora você é artista de artes proféticas, irá receber \
+        algumas palavras proféticas e deverá desenhar de acordo com o usuario dizer"
+        try:
+            image_url = generate_image(prompt_engineer, user_input)
+            if image_url:
+                st.write("Aqui está a imagem que o espirito santo te mostrou, se não gostar, recarregue a página e tente novamente")
+                st.image(image_url, width=700)
+            else:
+                st.write("Não foi possível gerar a imagem. Por favor, tente novamente.")
+        except Exception as e:
+            st.write(f"Ocorreu um erro ao gerar a imagem: {e}")
